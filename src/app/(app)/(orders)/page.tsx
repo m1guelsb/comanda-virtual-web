7b990460b2c942baf0ae9b6340c4f24135ca4934
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-29 18:08:01 -0300

    [fix] orders - remove isLoading nonexistent prop

--- src/app/(app)/(orders)/page.tsx
+++ src/app/(app)/(orders)/page.tsx
@@ -5,11 +5,9 @@ import { useListOrder } from '@/hooks/api/orders';
 import { useOrdersColumns } from '@/hooks/helpers';
 
 export default function Orders() {
-  const { orderList, isLoading } = useListOrder();
+  const { orderList } = useListOrder();
 
   const { ordersColumns } = useOrdersColumns({ orderList });
 
-  return (
-    <DnDOrdersColumns ordersColumns={ordersColumns} isLoading={isLoading} />
-  );
+  return <DnDOrdersColumns ordersColumns={ordersColumns} />;
 }
